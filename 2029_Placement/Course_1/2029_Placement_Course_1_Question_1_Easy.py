fdc17f689036b5bcb1320f01babb57c46da6dd0d
ursriram01-cyber <ursriram01@gmail.com>
2026-07-22 01:09:18 +0530

    ✅ [Accepted] 2029 Placement Course 1 Question 1 Easy (Python)

---
+++ 2029_Placement/Course_1/2029_Placement_Course_1_Question_1_Easy.py
@@ -0,0 +1,76 @@
+'''
+Problem Description:
+Description
+
+Write a program to display each digit separately in a given number
+
+For example:
+
+341
+
+3
+4
+1
+
+56420
+5
+6
+4
+2
+0
+
+Output
+	INPUT	EXPECTED	GOT	
+	
+341
+	
+3
+4
+1
+	
+3
+4
+1
+	
+	
+56420
+	
+5
+6
+4
+2
+0
+	
+5
+6
+4
+2
+0
+	
+
+Passed all tests!  
+
+Code
+c
+cpp
+java
+python3
+perl
+php
+ruby
+javascript
+c#
+golang
+Blocks
+Skip Quiz navigation
+1
+2
+3
+4
+5
+Finish attempt ...
+Run Code
+Ace editor not ready. Perhaps reload page?
+Falling back to raw text area.
+'''
+
